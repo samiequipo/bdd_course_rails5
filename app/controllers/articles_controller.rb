5fa4fdd5931ef7490b3f4cd2fb0c5117c975dfc7
samuel <samiequipo@gmail.com>
2021-03-16 18:49:54 -0400

    Create Article Successfully

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
-  
+    @article = Article.new
   end
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article has been created"
+      redirect_to articles_path
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end 
 end
