fde4a2b2d4e8ef81c5ec2a909272aa5a5011a49c
samuel <samiequipo@gmail.com>
2021-03-17 12:15:23 -0400

    flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(article_params) 
+    
     if @article.save
-      flash[:notice] = "Article has been created"
-      redirect_to articles_path
+      flash[:success] = "Article has been created"
+      redirect_to articles_path 
+    else
+      flash.now[:danger] = "Article has not been created"
+      render :new
     end
   end
-
+  
   private
     def article_params
       params.require(:article).permit(:title, :body)
