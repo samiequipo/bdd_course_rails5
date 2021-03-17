129db44d4534d53ac8b1d920eb8bd69be78ec78c
samuel <samiequipo@gmail.com>
2021-03-17 16:27:11 -0400

    Implement Showing Article Details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show]
+  
   def index
     @articles = Article.all
   end
 
   def new
     @article = Article.new
+  end
+
+  def show
   end
 
   def create
@@ -23,4 +28,8 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body)
     end 
+    
+    def set_article
+      @article = Article.find(params[:id]) 
+    end
 end
