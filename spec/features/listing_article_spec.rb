d3210f2cab740bb4abd93c590d26b1f0dc2cada6
samuel <samiequipo@gmail.com>
2021-03-19 12:43:48 -0400

    User cannot see >New article< if not sign in or sign up

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,14 +3,28 @@ require "rails_helper"
 RSpec.feature "Listing Articles" do
   before do
     john = User.create! email: "john@example.com", password: "password"
-    login_as(john)
                     
     @article1 = Article.create! title: "The first article", body: "Lorem ipsum dolor sit, amet",
                                 user: john  
     @article2 = Article.create! title: "The second article", body: "Body of 2nd article",
                                 user: john 
   end
-
+  
+  scenario "with articles created and user not signed in" do 
+    
+    visit "/"
+ 
+    expect(page).to have_content(@article1.title) 
+    expect(page).to have_content(@article1.body) 
+    expect(page).to have_content(@article2.title) 
+    expect(page).to have_content(@article2.body) 
+ 
+    expect(page).to have_link(@article1.title) 
+    expect(page).to have_link(@article2.title) 
+ 
+    expect(page).not_to have_link("New Article")
+  end
+  
   scenario "A user lists all articles" do
     visit '/'
 
